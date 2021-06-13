08d9817ace64c42fd3062e56a000113c3bf02b60
nafis-tech <nafiezlela9@gmail.com>
2021-06-13 15:21:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>To Do List</h1>
+            </div>
+        
+        )
+    }
 }
+export default App
 
-export default App;
+//klo mau import lagi, di apps di masukin navbar (klo mau ngasih navbar)
+//inget return harus cuma ada 1 kompnent saja
